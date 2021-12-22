a9f3dac1d78c587a2ec930c3bc2b42e3e3bc59c9
arnquist010 <arnquist010@gmail.com>
2021-12-21 21:51:54 -0600

    AdminPage graph added. AdminUserItem mapping done on AdminPage.

--- src/components/jsFiles/AdminUserItem.js
+++ src/components/jsFiles/AdminUserItem.js
@@ -1,9 +1,10 @@
 import '../cssFiles/AdminUserItem.css';
 
-function AdminUserItem() {
+function AdminUserItem(props) {
     return (
         <div className='AdminUserItem'>
-
+            <h4>{props.lastName}, {props.firstName}</h4>
+            <h4>{props.email}</h4>
         </div>
     )
 }
