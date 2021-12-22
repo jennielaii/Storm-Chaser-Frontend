aa5116829402bfcf5340c0a369bb6b5451882c98
typeluis <typeluis@gmail.com>
2021-12-21 22:58:31 -0500

    fixing landing page

--- src/pages/jsFiles/LandingPage.js
+++ src/pages/jsFiles/LandingPage.js
@@ -10,7 +10,7 @@ function LandingPage() {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
-    const [region, setRegion] = useState('')
+    const [region, setRegion] = useState("")
 
     const submitForm = async (e) => {
         try {
@@ -20,11 +20,11 @@ function LandingPage() {
                 firstName, lastName, email, region
             })
 
-            console.log(response)
+            console.log(response.data.user)
 
-            // await setUser(response.data.user)
+            // setUser(response.data.user)
 
-            // await localStorage.setItem('userId', response.data.user.id)
+            // localStorage.setItem('userId', response.data.user.id)
 
         } catch (error) {
             console.log(error)
@@ -35,38 +35,48 @@ function LandingPage() {
             <img alt='Storm Chasers Logo' />
             {/* <Link>Login</Link> */}
 
-            <div>
+            <div className='videoAndForm'>
                 {/* for youtube, replace "watch?v=" with "embed/" */}
-                <iframe src='https://www.youtube.com/embed/Xjv1sY630Uc'></iframe>
-                <form onSubmit={submitForm}>
+                <div className='form-video'>
+                    <iframe src='https://www.youtube.com/embed/Xjv1sY630Uc'></iframe>
+                </div>
+                
+                <form onSubmit={submitForm} className='signup-form'>
 
-                    <div>
-                        <label htmlFor="firstName">First Name:</label>
-                        <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+
+                    <div className='email-div'>
+                        <label htmlFor="email"></label>
+                        <input placeholder='Enter your Email Address' value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
 
-                    <div>
-                        <label htmlFor="lastName">Last Name:</label>
-                        <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                    <div className='name-div'>
+                        <label htmlFor="firstName"></label>
+                        <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+
+                        <label htmlFor="lastName"></label>
+                        <input placeholder='First Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
                     </div>
 
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <div className='select-div'>
+                        <select onChange={(e) => {setRegion(e.target.value)}} name='region' id='region-list'>
+                            {/* https://stackoverflow.com/questions/17603055/placeholder-for-select-tag */}
+                            <option value="" disabled selected hidden> What region are you located in?</option>
+                            <option value=""></option>
+                            <option value='North-West'>North-West</option>
+                            <option value='West'>West</option>
+                            <option value='Mid-West'>Mid-West</option>
+                            <option value='South-West'>South-West</option>
+                            <option value='South-East'>South-East</option>
+                            <option value='Mid-Atlantic'>Mid-Atlantic</option>
+                            <option value='North-East'>North-East</option>
+                        </select>
                     </div>
+                    
 
-
-                    <select onChange={(e) => {setRegion(e.target.value)}} name='region' id='region-list'>
-                        <option value='North-West'>North-West</option>
-                        <option value='West'>West</option>
-                        <option value='Mid-West'>Mid-West</option>
-                        <option value='South-West'>South-West</option>
-                        <option value='South-East'>South-East</option>
-                        <option value='Mid-Atlantic'>Mid-Atlantic</option>
-                        <option value='North-East'>North-East</option>
-                    </select>
-
-                    <input type="submit" value="Sign Up!" />
+                    <div className='submit-div'>
+                        <input type="submit" value="Subscribe Now!!" />
+                    </div>
+                    
                 </form>
             </div>
         </div>
