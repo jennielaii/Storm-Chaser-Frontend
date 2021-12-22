9af1fd8d58d98e36b8d73897b39b9fd6ccb0603f
arnquist010 <arnquist010@gmail.com>
2021-12-21 18:23:55 -0600

    Initialization and install completed. File structure created for pages and components. Routes added to 'App.js'.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import LandingPage from './pages/jsFiles/LandingPage';
+import AdminPage from './pages/jsFiles/AdminPage';
+import Footer from './components/jsFiles/Footer';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/admin' element={<AdminPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/jsFiles/AdminUserItem.js
@@ -0,0 +1,11 @@
+import '../cssFiles/AdminUserItem.css';
+
+function AdminUserItem() {
+    return (
+        <div className='AdminUserItem'>
+
+        </div>
+    )
+}
+
+export default AdminUserItem;
